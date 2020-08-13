6ed8ea414cae1ff016e8f0e95a9c14969487925e
Sean Urgel <seantheurgel@gmail.com>
2020-08-13 16:34:27 +0800

    Initial Commit

---
+++ 100 Days of Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  100 Days of Swift UI
+//
+//  Created by Sean Stuart C Urgel on 8/13/20.
+//  Copyright © 2020 Sean Urgel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
