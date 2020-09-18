16b5d6cabf41075aaadef7f0b251302480970599
Sean Urgel <seantheurgel@gmail.com>
2020-09-18 19:52:11 +0800

    Added Astronaut

---
+++ 100 Days of Swift UI/Project 8/Models/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  100 Days of Swift UI
+//
+//  Created by Sean Stuart C Urgel on 8/30/20.
+//  Copyright © 2020 Sean Urgel. All rights reserved.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
